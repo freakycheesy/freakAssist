025008b4a8b5c85299cc6afdeccee3734269e365
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-12-12 18:01:09 +0000

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using BoneLib.BoneMenu;
+using Newtonsoft.Json;
+using MelonLoader;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[assembly: MelonInfo(typeof(freakAssist.Main), "freakAssist", "1.0.0", "freakycheesy", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+namespace freakAssist
+{
+    public class Main : MelonMod
+    {
+        public static Main Instance;
+        public Page Page;
+        public Page ModsPage;
+        public const string urlPath = "https://github.com/freakycheesy/freakycheesy.github.io/raw/refs/heads/main/";
+        public Dictionary<string, ModMetadata> Mods = new();
+        public override void OnInitializeMelon()
+        {
+            Instance = this;
+            Page = Page.Root.CreatePage("FreakAssist", Color.green);
+            Page.CreateFunction("Refresh", Color.green, () => MelonCoroutines.Start(RequestMods()));
+            ModsPage = Page.CreatePage("MODS LIST", Color.green);
+            LoggerInstance.Msg("Initialized.");
+            MelonCoroutines.Start(RequestMods());
+        }
+
+        private IEnumerator RequestMods() {
+            UnityWebRequest www = UnityWebRequest.Get($"{urlPath}mods.json");
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success) {
+                MelonLogger.Error(www.error);
+            }
+            else {
+                // Show results as text
+                var json = www.downloadHandler.text;
+                MelonLogger.Msg(www.downloadHandler.text);
+                Mods = JsonConvert.DeserializeObject<Dictionary<string, ModMetadata>>(json);
+                CreateMods();
+            }
+        }
+        private void CreateMods() {
+            ModsPage.RemoveAll();
+            foreach (var mod in Mods) {
+                if (mod.Value.Game == Application.productName)
+                    continue;
+                var modPage = ModsPage.CreatePage(mod.Key, Color.green);
+                modPage.CreateFunction($"Version: {mod.Value.Version}", Color.green, null);
+                modPage.CreateFunction($"Author: {mod.Value.Author}", Color.green, null);
+                modPage.CreateFunction($"Description: {mod.Value.Description}", Color.green, null);
+                modPage.CreateFunction("Download", Color.green, ()=>mod.Value.DownloadMod(mod.Key + ".dll", urlPath));
+            }
+        }
+    }
+}
---
+++ ModMetadata.cs
@@ -0,0 +1,46 @@
+﻿using BoneLib.Notifications;
+using MelonLoader;
+using MelonLoader.Utils;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace freakAssist {
+    public struct ModMetadata {
+        public string Version;
+        public string Author;
+        public string Game;
+        public string ModLocation;
+        public string IconLocation;
+        public string Description;
+
+        public void DownloadMod(string fileName, string url) {
+            var fileUrl = url + ModLocation;
+            MelonCoroutines.Start(DownloadModRoutine(fileName, fileUrl));
+        }
+
+        private IEnumerator DownloadModRoutine(string fileName, string fileUrl) {
+            Notification noti = new();
+            noti.ShowTitleOnPopup = true;
+            noti.Title = "FreakAssist";
+            noti.Message = $"Downloading Mod {fileName}";
+            Notifier.Send(noti);
+            MelonLogger.Msg(noti.Message);
+
+            UnityWebRequest downloadRequest = UnityWebRequest.Get(fileUrl);
+            
+            downloadRequest.downloadHandler = new DownloadHandlerBuffer();
+            yield return downloadRequest.SendWebRequest();
+            if (downloadRequest.result != UnityWebRequest.Result.Success) {
+                noti.Message = $"Error Downloading Mod\n{downloadRequest.error}";
+            }
+            else {
+                var path = Path.Combine(MelonEnvironment.ModsDirectory, fileName);
+                DownloadHandlerBuffer buffer = downloadRequest.downloadHandler.TryCast<DownloadHandlerBuffer>();
+                File.WriteAllBytes(path, buffer.data);
+                noti.Message = $"Downloaded Mod\n{path} (pls restart the gaem)";
+            }
+            Notifier.Send(noti);
+            MelonLogger.Msg(noti.Message);
+        }
+    }
+}
